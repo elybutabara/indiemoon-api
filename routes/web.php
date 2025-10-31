336df34d4dcddff68580646aadc4eb162dca6bca
elybutabara <elybutabara@gmail.com>
2025-10-31 14:20:28 +0800

    add migration and config files, added league/flysystem-aws package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-upload', function () {    
+
+    try {
+        Storage::disk('s3')->put('new-folder/hello2.txt', 'Hello from Laravel via Supabase S3 new file');
+
+        $url = Storage::disk('s3')->temporaryUrl('new-folder/hello2.txt', now()->addMinutes(5));
+
+        return response()->json(['url' => $url], 200);
+    } catch (\Throwable $e) {
+        report($e);
+        return response()->json([
+            'error' => $e->getMessage(),
+            'class' => get_class($e),
+        ], 500);
+    }
 });
